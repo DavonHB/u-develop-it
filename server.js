886da028a0156dddd88b91f38916cabdc5459001
Nosferatu <davonb113@gmail.com>
2022-04-25 17:12:12 -0500

    added express server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
